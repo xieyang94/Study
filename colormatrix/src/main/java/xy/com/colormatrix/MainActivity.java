44f783638269aca93e55088dd4272ce60a7410dc
13260922711 <1192171830@qq.com>
2017-06-15 16:59:28 +0800

    1，加一个Log(master)

--- colormatrix/src/main/java/xy/com/colormatrix/MainActivity.java
+++ colormatrix/src/main/java/xy/com/colormatrix/MainActivity.java
@@ -14,6 +14,7 @@ import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Toast;
@@ -106,6 +107,8 @@ public class MainActivity extends AppCompatActivity {
 
         paint.setColorFilter(new ColorMatrixColorFilter(imageMarix));
         canvas.drawBitmap(bm, 0, 0, paint);
+
+        Log.d("TAG","master");
 
         return bmp;
     }
